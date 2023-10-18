01866b3a53b8655ef9c3cbe8c97c31df8d65aaa7
Dan Mac <danendraclever24@gmail.com>
2023-10-18 23:06:40 +0700

    second method work, but it's still oscilating

--- src/repl_only.cpp
+++ src/repl_only.cpp
@@ -2,19 +2,23 @@
 #include <cmath>
 #include "opencv2/opencv.hpp"
 #include <opencv2/highgui/highgui.hpp>
+#include "logger/logger.hpp"
 
 using namespace cv;
 using namespace std;
+
 // Constants
-const double kb = 1.0;  // For example, you can change this value
+const double ka = 2;    // For example
+const double kb = 21.0; // For example, you can change this value
 const double d0 = 1.0;  // For example
 const double m = 1.0;   // Mass of the robot
 const double dt = 0.01; // Time step
-const double ka = 1.0;  // For example
 
 // Image dimensions and scale factor for visualization
 const int imgSize = 500;
 const double scaleFactor = 50.0; // Adjust to fit the points within the image dimensions
+
+const double MAX_VELOCITY = 0.1; // Adjust this to set the maximum allowed speed
 
 // Function to compute distance between two Points
 double Distance(Point2f a, Point2f b)
@@ -27,7 +31,7 @@ Point2f ReplAccel(Point2f q, Point2f qc)
 {
     Point2f a_rep = {0, 0};
     double d = Distance(q, qc);
-    double factor = -kb * (1.0 / d - 1.0 / d0) / (d * d) / m;
+    double factor = kb * (1.0 / d - 1.0 / d0) / (d * d) / m;
     double norm = Distance(q, qc);
 
     a_rep.x = factor * (q.x - qc.x) / norm;
@@ -45,12 +49,23 @@ Point2f AttrAccel(Point2f q, Point2f qd)
     return a_att;
 }
 
+void limitVelocity(Point2f &v)
+{
+    double magnitude = Distance(v, {0, 0});
+    Logger(RED, "Magnitude: %f", magnitude);
+    if (magnitude > MAX_VELOCITY)
+    {
+        v.x = (v.x / magnitude) * MAX_VELOCITY;
+        v.y = (v.y / magnitude) * MAX_VELOCITY;
+    }
+}
+
 // Main function
 int main()
 {
     // Initial conditions
     Point2f q = {1, 1};  // Initial position of the robot
-    Point2f qc = {2, 2}; // Initial position of the closest obstacle
+    Point2f qc = {2, 1}; // Initial position of the closest obstacle
     Point2f v = {0, 0};  // Initial velocity
     Point2f qd = {4, 4}; // goal position
 
@@ -59,32 +74,49 @@ int main()
 
     circle(pathImg, Point(q.x * scaleFactor, q.y * scaleFactor), 2, Scalar(0, 0, 0), -1);
     circle(pathImg, Point(qc.x * scaleFactor, qc.y * scaleFactor), 2, Scalar(255, 0, 0), -1);
+    circle(pathImg, Point(qd.x * scaleFactor, qd.y * scaleFactor), 2, Scalar(0, 0, 255), -1);
 
     // Compute acceleration and integrate to get velocity
-    for (int step = 0; step < 100; ++step)
+    while (true)
     { // For example, 100 steps
         Point2f repl_accel = ReplAccel(q, qc);
-        // v.x += repl_accel.x * dt;
-        // v.y += repl_accel.y * dt;
-
+        // Point2f repl_accel = {0, 0};
         Point2f attr_accel = AttrAccel(q, qd);
+        // Point2f attr_accel = {0, 0};
 
-        v.x += attr_accel.x * dt;
-        v.y += attr_accel.y * dt;
+        Point a_total;
+        a_total.x = repl_accel.x + attr_accel.x;
+        a_total.y = repl_accel.y + attr_accel.y;
+
+        v.x += a_total.x * dt;
+        v.y += a_total.y * dt;
+
+        // limitVelocity(v);
+
+        Logger(RED, "Velocity: %f %f", v.x, v.y);
+
+        double speed = sqrt(v.x * v.x + v.y * v.y);
+
+        if (speed > MAX_VELOCITY)
+        {
+            v.x = (v.x / speed) * MAX_VELOCITY;
+            v.y = (v.y / speed) * MAX_VELOCITY;
+        }
 
         q.x += v.x;
         q.y += v.y;
 
         // For demonstration purposes, you can print the velocity
-        cout << "Velocity at step " << step << ": (" << v.x << ", " << v.y << ") "
-             << "On position: (" << q.x << ", " << q.y << ")" << endl;
+        // cout << "Velocity at step "
+        //      << ": (" << v.x << ", " << v.y << ") "
+        //      << "On position: (" << q.x << ", " << q.y << ")" << endl;
 
         // Draw the current position of the robot
         circle(pathImg, Point(q.x * scaleFactor, q.y * scaleFactor), 2, Scalar(0, 0, 0), -1);
         imshow("Robot Path", pathImg);
 
         // delay 500ms
-        waitKey(500);
+        waitKey(100);
     }
 
     waitKey(0);
